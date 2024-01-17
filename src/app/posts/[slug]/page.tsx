a844833666d722103852a7c7bf651783d76e8ae6
abe <abel.rogers@gmail.com>
2024-01-17 20:37:20 +0000

    Update page.tsx

--- src/app/posts/[slug]/page.tsx
+++ src/app/posts/[slug]/page.tsx
@@ -33,96 +33,84 @@ function truncateText(text: string, length: number): string {
   return text.length > length ? text.substring(0, length) + "..." : text;
 }
 
-async function fetchPostData<T>(
-  slug: string,
-  fn: (post: Post) => Promise<T>
-): Promise<T> {
-  const post: Post = await api.post.getPostBySlug.query({ slug });
-  return await fn(post);
-}
-
 export async function generateMetadata({ params }: Props): Promise<Metadata> {
   // read route params
   const slug = params.slug;
+  const post: Post = await api.post.getPostBySlug.query({ slug });
 
-  return fetchPostData(slug, async (post: Post) => {
-    let description = "";
-    if (post?.post_content) {
-      description = truncateText(post.post_content, 100);
-    }
+  let description = "";
+  if (post?.post_content) {
+    description = truncateText(post.post_content, 100);
+  }
 
-    return {
-      title: post?.post_title,
-      description: description,
-    };
-  });
+  return {
+    title: post?.post_title,
+    description: description,
+  };
 }
 
-const PostContent = ({ post }: { post: Post | null }) => {
-  if (post === null) {
-    return <p className={styles.showcaseText}>No post data found!</p>;
-  }
+export default async function PostBySlug({
+  params,
+}: {
+  readonly params: { slug: string };
+}) {
+  const slug = params.slug;
+  const post: Post = await api.post.getPostBySlug.query({ slug });
+
   return (
     <main className={styles.main}>
       <div className={styles.container}>
         <div className={styles.showcaseContainer}>
-          <h1 className={styles.title}>{post.post_title}</h1>
-          <Image
-            src={`/images/wp_dummy_content_generator_${post.ID}.jpg`}
-            alt={post.post_title}
-            width={400}
-            height={400}
-          />
-          <p>
-            Created {post.post_date.toLocaleDateString("pt-BR")}, Modified{" "}
-            {post.post_modified.toLocaleDateString("pt-BR")}
-          </p>
-          {/* display the wp_terms.name if taxonomy == category */}
-
-          {post.wp_term_relationships.map((relationship) =>
-            relationship.wp_term_taxonomy?.taxonomy === "category" &&
-            relationship.wp_term_taxonomy.wp_terms ? (
-              <p
-                key={relationship.wp_term_taxonomy.term_taxonomy_id}
-                className={styles.showcaseText}
-              >
-                Category: {relationship.wp_term_taxonomy.wp_terms.name}
+          {post !== null ? (
+            <>
+              <h1 className={styles.title}>
+                {(post as { post_title: string }).post_title}
+              </h1>
+              <Image
+                src={`/images/wp_dummy_content_generator_${post.ID}.jpg`}
+                alt={`${post.post_title}`}
+                width={400}
+                height={400}
+              />
+              <p>
+                Created {post.post_date.toLocaleDateString("pt-BR")}, Modified{" "}
+                {post.post_modified.toLocaleDateString("pt-BR")}
               </p>
-            ) : null
-          )}
+              {/* display the wp_terms.name if taxonomy == category */}
+              <>
+                {post.wp_term_relationships.map((relationship) =>
+                  relationship.wp_term_taxonomy?.taxonomy === "category" &&
+                  relationship.wp_term_taxonomy.wp_terms ? (
+                    <p
+                      key={relationship.wp_term_taxonomy.term_taxonomy_id}
+                      className={styles.showcaseText}
+                    >
+                      Category: {relationship.wp_term_taxonomy.wp_terms.name}
+                    </p>
+                  ) : null
+                )}
+              </>
 
-          <p className={styles.showcaseText}>
-            <br />
-            {post.post_content}
-          </p>
-          <h3>Raw Data</h3>
-          <pre className={styles.preBox}>
-            {JSON.stringify(
-              post,
-              (key, value) =>
-                // eslint-disable-next-line @typescript-eslint/no-unsafe-return
-                typeof value === "bigint" ? value.toString() : value,
-              4
-            )}
-          </pre>
+              <p className={styles.showcaseText}>
+                <br />
+                {(post as { post_content: string }).post_content}
+              </p>
+              <h3>Raw Data</h3>
+              <pre className={styles.preBox}>
+                {JSON.stringify(
+                  post,
+                  (key, value) =>
+                    // eslint-disable-next-line @typescript-eslint/no-unsafe-return, @typescript-eslint/no-unnecessary-type-assertion, @typescript-eslint/no-redundant-type-constituents
+                    typeof value === "bigint" ? value.toString() : value,
+                  4
+                )}
+              </pre>
+            </>
+          ) : (
+            <p className={styles.showcaseText}>Loading post...</p>
+          )}
         </div>
       </div>
     </main>
   );
-};
-
-export default function PostBySlug({
-  params,
-}: {
-  readonly params: { slug: string };
-}): Promise<JSX.Element> {
-  const slug = params.slug;
-
-  return fetchPostData(slug, async (post: Post) => {
-    return post ? (
-      <PostContent post={post} />
-    ) : (
-      <p className={styles.showcaseText}>Loading post...</p>
-    );
-  });
 }
